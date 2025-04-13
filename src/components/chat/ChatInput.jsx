20dcf5b128d8d0972db7ed2a31744cb22da00b2b
Jeongeun Park <wjddms@swu.ac.kr>
2025-04-13 22:15:15 +0900

    로컬 kafka 테스트 전용 /chat 개발

---
+++ src/components/chat/ChatInput.jsx
@@ -0,0 +1,35 @@
+// components/chat/ChatInput.jsx
+
+import React, { useState } from "react";
+import "./ChatInput.css";
+
+/* assets */
+import sendBtn from "../../assets/images/sendBtn.svg";
+
+const ChatInput = ({ onSend }) => {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!message.trim()) return;
+
+        onSend(message); // 외부에서 받은 콜백으로 메시지 전달
+        setMessage("");
+    };
+
+    return (
+        <form className="chat-input" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="메시지를 입력하세요..."
+            />
+            <button type="submit" className="send-button">
+                <img src={sendBtn} alt="보내기" />
+            </button>
+        </form>
+    );
+};
+
+export default ChatInput;
---
+++ src/components/chat/ChatMessage.jsx
@@ -0,0 +1,31 @@
+// components/chat/ChatMessage.jsx
+
+import React from "react";
+import "./ChatMessage.css";
+
+/* assets */
+import botImg from "../../assets/images/itchingSymbol.svg"
+
+const ChatMessage = ({ sender, text }) => {
+    const isBot = sender === "bot";
+
+    return (
+        <div className={`chat-message ${sender}`}>
+            {isBot && (
+                <div className="chat-profile">
+                    <div className="chat-avatar-bg">
+                        <img
+                            className="chat-avatar"
+                            src={botImg} // 봇 이미지 경로
+                            alt="챗봇 프로필"
+                        />
+                    </div>
+                    <span className="chat-name">잇칭 챗봇</span>
+                </div>
+            )}
+            <div className="message-bubble">{text}</div>
+        </div>
+    );
+};
+
+export default ChatMessage;
