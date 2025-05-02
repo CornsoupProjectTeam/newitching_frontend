7258a261e7410edcd9430c3347d788dd617352c4
Jeongeun Park <wjddms@swu.ac.kr>
2025-05-03 02:28:51 +0900

    App.js 병합 오류 수정

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainHeader from "./components/header/MainHeader";
 import Footer from "./components/footer/Footer";
 import Chat from "./pages/Chat/ChatPage"
-import RegisterMatch from "./pages/RegisterProject/RegisterMatch";
-import RegisterStart from "./pages/RegisterProject/RegisterStart";
-import RegisterUrl from "./pages/RegisterProject/RegisterUrl";
+import MatchingPage from "./pages/RegisterProject/MatchingPage";
+import MatchingUrl from "./pages/RegisterProject/MatchingUrl";
 import MemberRegister from "./components/chat/MemberRegister";
+import MatchResultSignIn from "./pages/MatchResultProject/MatchResultSignIn";
+import HomePage from "./pages/HomePage";
 import Big5ResultPage from "./pages/Big5/Big5Page"
 import MatchingResultPage from "./pages/TeamMatchingResult/MatchingResultPage";
 
@@ -18,10 +19,11 @@ const App = () => {
           <MainHeader />
           <Routes>
               <Route path="/chat" element={<Chat />} />
-              <Route path="/registermatch" element={<RegisterMatch />} />
-              <Route path="/registerstart" element={<RegisterStart />} />
-              <Route path="/registerurl" element={<RegisterUrl />} />
+              <Route path="/matching" element={<MatchingPage />} />
+              <Route path="/matching/register" element={<MatchingUrl />} />
               <Route path="/memberregister" element={<MemberRegister />} />
+              <Route path="/matchresultsignin" element={<MatchResultSignIn />} />
+              <Route path="/" element={<HomePage />} />
               <Route path="/big5" element={<Big5ResultPage />} />
               <Route path="/matching/result" element={<MatchingResultPage />} />
           </Routes>
